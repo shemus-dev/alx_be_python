5b59b8a7f18fe9b6ba836e251a570b828e722991
shemus-dev <shemmutuku4@gmail.com>
2025-09-08 14:03:17 +0300

    updates

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+current_age = int(input("How old are you"))
+
+yearstoadd = 2050 - 2023
+agein25 = current_age + yearstoadd
+print(f"In 2050, you will be {agein25} years old.")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+hours = 2
+
+
+seconds = hours * 3600
+
+print(f"{hours} hour(s) is {seconds} seconds.")
