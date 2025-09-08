b34b89dc9c83a480cd88b944fa871b63d0dd85cf
shemus-dev <shemmutuku4@gmail.com>
2025-09-08 13:26:49 +0300

    simple interst added

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+principal = 1000     # $1000
+rate = 0.05          # 5% annual interest
+time = 3             # 3 years
+
+# Calculate simple interest
+interest = principal * rate * time
+
+# Print result
+print(f"The simple interest is: {interest}")
