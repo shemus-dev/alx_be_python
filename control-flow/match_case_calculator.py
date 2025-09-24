093b92abbf45c94a80ce87d34c50c6affb7b8d70
shemus-dev <shemmutuku4@gmail.com>
2025-09-24 17:27:34 +0300

    push

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = int(input("Enter the first number: "))
 num2  = int(input("Enter the second number:"))
-perform = input("which type of operation,would you like to use: (+, -, *, / )")
+perform = input("Choose the operation (+, -, *, /):")
 
 match perform:
     case "+":
