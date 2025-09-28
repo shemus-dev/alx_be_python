ff05a35534c25eb8df0e1f6b2b5775fa975e87e7
shemus-dev <shemmutuku4@gmail.com>
2025-09-28 22:17:19 +0300

    addthis

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,5 @@
-def perform_operation(num1,num2, operation):
+def perform_operation(num1, num2, operation):
+
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
@@ -7,7 +8,7 @@ def perform_operation(num1,num2, operation):
         return num1 * num2
     elif operation == "divide":
         if num2 == 0:
-            return  "Error: Division by zero"
+            return "Error: Division by zero"
         return num1 / num2
     else:
         return "Error: Invalid operation"
