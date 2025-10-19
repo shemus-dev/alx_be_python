b362fe4007f9dfcc75caca8f160d5e45a851ed40
shemus-dev <shemmutuku4@gmail.com>
2025-10-19 22:03:14 +0300

    Add MySQL schema for alx_book_store database

--- Intro_to_DB/alx_book_store.sql
+++ Intro_to_DB/alx_book_store.sql
@@ -1,40 +1,39 @@
-CREATE DATABASE ALX_BOOK_STORE;
-USE ALX_BOOK_STORE;
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
 
-CREATE TABLE AUTHORS (
-    author_id int auto_increment PRIMARY KEY,
+CREATE TABLE authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
-CREATE  TABLE BOOKS   (
-    book_id INT auto_increment PRIMARY KEY,
-    title VARCHAR(130),
+CREATE TABLE books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    Foreign Key (author_id) REFERENCES AUTHORS(author_id)
-
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
-CREATE TABLE CUSTOMERS  (
-    customer_id INT auto_increment PRIMARY KEY,
+CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE Table ORDERS  (
-    order_id INT auto_increment PRIMARY KEY,
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    Foreign Key (customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
-CREATE TABLE ORDER_DETAILS(
-    orderdetailid INT auto_increment PRIMARY KEY,
+CREATE TABLE order_details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
